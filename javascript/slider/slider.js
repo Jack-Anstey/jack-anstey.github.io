385c735ad260827a921e3803587b890814b81559
Jack-Anstey <jackajacka@outlook.com>
2022-11-16 19:04:47 -0500

    Getting closer to having a functional two dot slider

--- javascript/slider/slider.js
+++ javascript/slider/slider.js
@@ -5,14 +5,14 @@ window.onload = function(){
     slideOne();
     slideTwo();
 }
-let sliderOne = document.getElementById("slider-1");
-let sliderTwo = document.getElementById("slider-2");
+let sliderOne = document.getElementById("slider1");
+let sliderTwo = document.getElementById("slider2");
 let displayValOne = document.getElementById("range1");
 let displayValTwo = document.getElementById("range2");
 let minGap = 0; //zero because our ranges are inclusive
-let sliderTrack = document.querySelector(".slider-track");
-let sliderMaxValue = document.getElementById("slider-1").max;
-let sliderMinValue = document.getElementById("slider-1").min; //for making a slider with a different min than 0
+let sliderTrack = document.querySelector(".sliderTrack");
+let sliderMaxValue = document.getElementById("slider1").max;
+let sliderMinValue = document.getElementById("slider1").min; //for making a slider with a different min than 0
 function slideOne(){
     if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
         sliderOne.value = parseInt(sliderTwo.value) - minGap;
