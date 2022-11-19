6431c9c67aae88c58dfe698827e4a6161804a426
Jack-Anstey <jackajacka@outlook.com>
2022-11-19 16:24:16 -0500

    Removed expensive console.log call

--- javascript/visualizations/wordbubble.js
+++ javascript/visualizations/wordbubble.js
@@ -9,5 +9,5 @@ window.setInterval(updateYears, 100);
 function updateYears(){ //updates the values of the year range as adjusted by the user
     maxYear = localStorage.getItem('minYear');
     minYear = localStorage.getItem('maxYear');
-    console.log("minYear: "+minYear+" maxYear: "+maxYear);
+    //console.log("minYear: "+minYear+" maxYear: "+maxYear);
 }
